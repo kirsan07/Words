9643f952c28c2324d01fb328c99538b282e03e86
kirsan07 <terkirsun@gmail.com>
2023-01-23 19:38:48 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,63 @@ public class Main {
         String[] massive = string.split("[\\s]");
         String[] words = string.split("[\\s]+");
         String letters = String.join("", words);
-        String[] sentence = string.split("[\\W]");
-        System.out.println("words" + " " + massive.length);
-        System.out.println("letter " + " " + letters.length());
-        System.out.println("sentence " + sentence.length);
-
+        String[] sentence = string.split("[\\.]");
+        System.out.println("Words:" + " " + massive.length);
+        System.out.println("Characters: " + " " + letters.length());
+        System.out.println("Sentence: " + sentence.length);
+        int score = 0;
+        int drob1 = letters.length();
+        int drob2 = words.length;
+        int drob3 = sentence.length;
+        double result = 4.71 * drob1 / drob2 + 0.5 * drob2 / drob3 - 21.43;
+        System.out.println("The score is: " + result);
+        int i = 0;
+        while (i < result) {
+            i++;
+        }
+        i = i-1;
+        if (i == 1) {
+            System.out.println("This text should be understood by 5-6 year-olds");
+        }
+        if (i == 2) {
+            System.out.println("This text should be understood by 6-7 year-olds");
+        }
+        if (i == 3) {
+            System.out.println("This text should be understood 7-9 year-olds");
+        }
+        if (i == 4) {
+            System.out.println("This text should be understood by 9-10 year-olds");
+        }
+        if (i == 5) {
+            System.out.println("This text should be understood by 10-11 year-olds");
+        }
+        if (i == 6) {
+            System.out.println("This text should be understood by 11-12 year-olds");
+        }
+        if (i == 7) {
+            System.out.println("This text should be understood by 12-13 year-olds");
+        }
+        if (i == 8) {
+            System.out.println("This text should be understood by 13-14 year-olds");
+        }
+        if (i == 9) {
+            System.out.println("This text should be understood by 14-15 year-olds");
+        }
+        if (i == 10) {
+            System.out.println("This text should be understood by 15-16 year-olds");
+        }
+        if (i == 11) {
+            System.out.println("This text should be understood by 16-17 year-olds");
+        }
+        if (i == 12) {
+            System.out.println("This text should be understood by 17-18 year-olds");
+        }
+        if (i == 13) {
+            System.out.println("This text should be understood by 18-19 year-olds");
+        }
+        if (i == 14) {
+            System.out.println("This text should be understood by 24+ year-olds");
+        }
     }
 }
 
